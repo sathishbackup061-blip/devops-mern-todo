aecded4d35155b5d2f7c568a411c32b4bcd766a1
sathishbackup061-blip <sathiishbackup061@gmail.com>
2026-06-08 08:11:02 +0530

    update the port

--- backend/index.js
+++ backend/index.js
@@ -26,9 +26,9 @@ if (process.env.NODE_ENV !== "test") {
 
   const PORT = process.env.PORT || 5000;
 
-    app.listen(PORT, "0.0.0.0", () => {
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-    });
+  });
 }
 
 module.exports = app;
