c9f09d30abdafaba9b87d947cffc5302aa33d28a
sathishbackup061-blip <sathiishbackup061@gmail.com>
2026-06-08 08:00:12 +0530

    added server port

--- backend/index.js
+++ backend/index.js
@@ -26,9 +26,9 @@ if (process.env.NODE_ENV !== "test") {
 
   const PORT = process.env.PORT || 5000;
 
-  app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
-  });
+    });
 }
 
 module.exports = app;
